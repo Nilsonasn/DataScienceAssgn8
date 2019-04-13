f801ffc8db8c7144e252f0eea1fbd930071ef6e8
Nilsonasn <Mr.andrew.nilson@gmail.com>
2019-04-13 10:44:49 -0600

    modified the Group By Company
    Group by company now takes 3 args: ReadFile, If you want to Write or not, and the writefile

--- Andrew Methods/GroupByCompany.py
+++ Andrew Methods/GroupByCompany.py
@@ -1,16 +1,18 @@
 import pandas as pd
 
-def GroupByCompany():
-    df = pd.read_csv("../Stocks/Nasdaq-Friday-April-20-2018.csv")
+def GroupByCompany(readFile = "",write=False,writeFile=""):    
+    df = pd.read_csv(readFile)
     companies = df['Symbol'].values
     companies = list(dict.fromkeys(companies))
     i = 0
     companyDF = []
     while (i<len(companies)):
         companyDF.append(df[df['Symbol']==companies[i]])
-        companyDF[i].to_csv('../Stocks_By_Company/'+companies[i]+'.csv')
+        if(write==True and not writeFile == ""):
+            companyDF[i].to_csv(writeFile+'/'+companies[i]+'.csv')
         i = i +1
-    print(companyDF[1])
+    return companyDF
     
-    
-GroupByCompany() 
+GroupByCompany(readFile = "../Stocks/Nasdaq-Friday-April-20-2018.csv",write = False,writeFile = "../Stocks_By_Company")
+
+
